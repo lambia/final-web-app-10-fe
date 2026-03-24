fcb3cf9a60c63d12049f9e9da93f07be252b50bb
Luca Lambia <luca@lambia.it>
2026-03-24 21:25:36 +0100

    uso assets da backend

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -4,9 +4,10 @@ function BookCard({ book }) {
 
 	//Stesse proprietà con cui mi verrebbe restituito dalla webapi
 	const { id, title, author, abstract, image } = book;
+	const apiAssetsUrl = "http://localhost:3000/static/books";
 
 	return <div className="card">
-		<img src={image} alt={title} />
+		<img src={`${apiAssetsUrl}/${image}`} alt={title} />
 		<h2>{title}</h2>
 		<h3>{author}</h3>
 		<p>{abstract}</p>
