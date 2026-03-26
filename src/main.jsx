6cdc4bd91ab11d780bc6d8517e3be9697c296067
Luca Lambia <luca@lambia.it>
2026-03-26 21:31:01 +0100

    disattivo strict mode (TEMPORANEO!)

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	// Ricordatevi di riattivare lo strict mode dopo aver testato le insert!!!
+	//   <StrictMode>
+	<App />
+	//   </StrictMode>,
 )
