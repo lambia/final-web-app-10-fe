7725b122c2abf8966550fccb7a0ee20a98b504bf
Luca Lambia <luca@lambia.it>
2026-03-26 19:12:37 +0100

    stelline recensione

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,9 +3,24 @@ function ReviewCard({ review }) {
 	//Stesse proprietà con cui mi verrebbe restituito dalla webapi
 	const { name, vote, text } = review;
 
+
+
 	return <div className="card">
 		<h2>{name}</h2>
-		<h3>{vote}/5</h3>
+
+		<div>
+			{[1, 2, 3, 4, 5].map(elemento => {
+				return <i className={"fa-star " + (vote >= elemento ? "fa-solid" : "fa-regular")}></i>
+			})}
+
+
+			{/* <i className={"fa-star " + (vote >= 1 ? "fa-solid" : "fa-regular")}></i>
+			<i className={"fa-star " + (vote >= 2 ? "fa-solid" : "fa-regular")}></i>
+			<i className={"fa-star " + (vote >= 3 ? "fa-solid" : "fa-regular")}></i>
+			<i className={"fa-star " + (vote >= 4 ? "fa-solid" : "fa-regular")}></i>
+			<i className={"fa-star " + (vote >= 5 ? "fa-solid" : "fa-regular")}></i> */}
+		</div>
+
 		<p>{text}</p>
 	</div>
 }
