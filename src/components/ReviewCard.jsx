20c8a5b2136cc568ced025b51f78ae07e02a5923
Luca Lambia <luca@lambia.it>
2026-03-26 19:15:37 +0100

    minor stelline

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -9,16 +9,17 @@ function ReviewCard({ review }) {
 		<h2>{name}</h2>
 
 		<div>
-			{[1, 2, 3, 4, 5].map(elemento => {
-				return <i className={"fa-star " + (vote >= elemento ? "fa-solid" : "fa-regular")}></i>
-			})}
-
-
 			{/* <i className={"fa-star " + (vote >= 1 ? "fa-solid" : "fa-regular")}></i>
 			<i className={"fa-star " + (vote >= 2 ? "fa-solid" : "fa-regular")}></i>
 			<i className={"fa-star " + (vote >= 3 ? "fa-solid" : "fa-regular")}></i>
 			<i className={"fa-star " + (vote >= 4 ? "fa-solid" : "fa-regular")}></i>
 			<i className={"fa-star " + (vote >= 5 ? "fa-solid" : "fa-regular")}></i> */}
+
+			{[1, 2, 3, 4, 5].map(elemento => {
+				return <i className={"fa-star " + (vote >= elemento ? "fa-solid" : "fa-regular")}></i>
+			})}
+
+			{/* <ComponenteStelline voto={vote} /> */}
 		</div>
 
 		<p>{text}</p>
