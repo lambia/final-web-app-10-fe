836ad5a82b48aa40cd42e7f3a9600ab2899a802d
Luca Lambia <luca@lambia.it>
2026-03-26 21:32:19 +0100

    riattivo strict mode!

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-	// Ricordatevi di riattivare lo strict mode dopo aver testato le insert!!!
-	//   <StrictMode>
-	<App />
-	//   </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
